feb468e00cd05c2fc2424c322d6ae36d9315b9ca
JXSnack <jx.snack@gmail.com>
2024-11-15 23:08:31 +0100

    better handling

--- builder.go
+++ builder.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"strings"
 )
 
 func Mkmod(
@@ -19,7 +21,7 @@ func Mkmod(
 
 		switch command {
 		case "mkdir":
-			mkdir(element["name"].(string))
+			mkdir(element["name"].(string), name)
 			break
 		default:
 			fmt.Printf("\033[0;31mUnknown command: %s\033[0m\n", command)
@@ -28,8 +30,8 @@ func Mkmod(
 	}
 }
 
-func mkdir(path string) {
-	err := os.MkdirAll(path, 0755)
+func mkdir(dir string, name string) {
+	err := os.MkdirAll(path.Join(strings.Replace(name, "/", string(os.PathSeparator), -1), dir), 0755)
 	if err != nil {
 		panic(err)
 	}
