9572723d165a3547874fbf882bcbe7e036a04045
JXSnack <jx.snack@gmail.com>
2024-11-17 17:09:37 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script is used to quickly create all binaries, for both macOS and Windows
+ARCHS_OSX=("amd64" "arm64")
+ARCHS_WIN=("amd64" "arm64")
+
+mkdir -p target
+
+echo "Building macOS..."
+for ARCH in "${ARCHS_OSX[@]}"; do
+  echo "Building for darwin/$ARCH..."
+  GOOS=darwin GOARCH=$ARCH go build -o "target/latest-macOS-$ARCH"
+done
+
+echo "Building Windows..."
+for ARCH in "${ARCHS_WIN[@]}"; do
+  echo "Building for windows/$ARCH..."
+  GOOS=windows GOARCH=$ARCH go build -o "target/latest-windows-$ARCH.exe"
+done
+
+echo "Finished!"
